9ffe645d9a68bfe8d9108a19906e3bcdea712f39
Pavel-Tymoshchuk <38438426+Pavel-Tymoshchuk@users.noreply.github.com>
2020-01-24 18:02:08 +0200

    new elem

--- js/main.js
+++ js/main.js
@@ -109,6 +109,31 @@ document.addEventListener("DOMContentLoaded", function(event) {
     
     // /Popup
     
+    // Stars
+    
+    document.addEventListener('click', function(e){
+        let elem = e.target;
+        
+        if(elem.closest(".star__item")) {
+            let parent = elem.closest('.review_form__wrapper');
+            let starInput = parent.querySelector('.stars__input');
+            let starValuse = elem.closest(".star__item").getAttribute('data-target');
+            let allStarts = document.querySelectorAll('.star__item');
+            starInput.setAttribute('value', starValuse);
+            starList = parent.querySelector('.star__list');
+            
+            starList.classList.add('checked');
+            
+            allStarts.forEach(function(item){
+                item.classList.remove("active");
+            });
+            
+            elem.closest(".star__item").classList.add('active');
+        }
+    });
+    
+     // /Stars
+    
     //Drop list 
     
     var dropList = document.querySelectorAll('.sort_type');
@@ -155,22 +180,95 @@ document.addEventListener("DOMContentLoaded", function(event) {
     // General Slider 
     
     let arrows = document.querySelectorAll(".js-arrows");
-    let showItem = document.querySelector
     
+    
+    let arrowsInfinity = document.querySelectorAll('.js-arrows-infinity');
+
     function setCurrentSlideIndex(item) {
-        let currentSlideIndex = item.getAttribute('data-index');
-        let numberSlider = document.querySelector('.number-slider .current');
-        
-        numberSlider.innerHTML = currentSlideIndex;
+        let slider = item.closest('.slider-infinity');
+        let currentSlideIndex = slider.querySelector('.js-slider-item-infinity.show').getAttribute('data-index');
+        item.innerHTML = currentSlideIndex;
     }
-    
-    setCurrentSlideIndex(showItem);
-    
+
+    document.querySelectorAll('.js-arrows-infinity .current').forEach(function(item){
+        setCurrentSlideIndex(item);
+    });
+
+    function initialSliderInfinity() {
+        for(var i = 0;arrowsInfinity.length > i; i++) {
+            let slider = arrowsInfinity[i].closest(".slider-infinity");
+            let sliderItems = slider.querySelectorAll('.js-slider-item-infinity');
+            let arrowNext = arrowsInfinity[i].querySelector('.next');
+            let arrowPrev = arrowsInfinity[i].querySelector('.prev');
+            let sliderList = slider.querySelector('.js-slider-list');
+            let allNumber = slider.querySelector('.js-arrows-infinity .all-slider');
+            
+            allNumber.innerHTML = sliderItems.length;
+            
+            arrowNext.addEventListener('click', function() {
+                let itemShow = slider.querySelector('.js-slider-item-infinity.show');
+                
+                itemShow.nextElementSibling.classList.add('show');
+                itemShow.classList.remove('show');
+                
+                setTimeout(function(){
+                    let newElem = itemShow;
+                    itemShow.remove();
+                    sliderList.append(newElem);
+                },750);
+                
+                document.querySelectorAll('.js-arrows-infinity .current').forEach(function(item){
+                    setCurrentSlideIndex(item);
+                });
+            });
+            
+            arrowPrev.addEventListener('click', function() {
+                let itemShow = slider.querySelector('.js-slider-item-infinity.show');
+                let lastElem = sliderList.lastElementChild;
+                
+                sliderList.prepend(lastElem);
+                
+                setTimeout(function(){
+                    itemShow.previousElementSibling.classList.add('show');
+                    itemShow.classList.remove('show');
+                    document.querySelectorAll('.js-arrows-infinity .current').forEach(function(item){
+                        setCurrentSlideIndex(item);
+                    });
+                },20)
+            });
+            
+            var startPointX;
+            var startPointY;
+            slider.addEventListener("touchstart", function(event) {
+                startPointX = event.changedTouches[0].screenX;
+                startPointY = event.changedTouches[0].screenY;
+            }, false);
+            slider.addEventListener("touchend", function(event){
+                var endPointX = event.changedTouches[0].screenX;
+                var endPointY = event.changedTouches[0].screenY;
+                
+                if(startPointX - endPointX > 40) {
+                    arrowNext.click();
+                } else if(endPointX - startPointX > 40) {
+                    arrowPrev.click();
+                }
+            }, false);
+        }
+    }
+    initialSliderInfinity();
+        
     function initialSlider() {
         for(var i = 0;arrows.length > i; i++) {
             let slider = arrows[i].closest(".slider");
             let arrowNext = arrows[i].querySelector('.next');
             let arrowPrev = arrows[i].querySelector('.prev');
+            let allPage = slider.querySelector('.js-arrows .all-slider');
+            let allItems = slider.querySelectorAll('.js-slider-item').length;
+            
+            if(allPage) {
+                allPage.innerHTML = allItems;
+            }
+            
             
             arrowNext.addEventListener('click', function() {
                 let itemShow = slider.querySelector('.js-slider-item.show');
@@ -209,6 +307,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
                     dotActive.classList.remove('active');
                 }
                 
+                 
                 arrowNext.classList.remove('disable');
                 
                 itemShow.previousElementSibling.classList.add('show');
